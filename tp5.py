bc096087147b7877e66929704742c35613d8437a
Alexandre Binette <alexbinette@hotmail.fr>
2019-11-30 08:16:22 -0500

    add GET queries and establish the dataframe, print to CSV

--- tp5.py
+++ tp5.py
@@ -1,7 +1,18 @@
 """Travail Pratique 5 - Alexandre Binette."""
 
 import sys
+import json
+import pandas as pd
+from urllib.request import urlopen
 from Bio.Blast import NCBIXML
+
+def get_request(query_url):
+    """Returns the result of a URL GET in json format with urlopen"""
+
+
+    stream = urlopen(query_url)
+    result = json.loads(stream.read().decode())
+    return result
 
 # Importe le fichier au argv[1] et attrape les erreurs de filepath
 try:
@@ -11,17 +22,56 @@ except (IOError, IndexError):
     please specify filepath"""
     print(error_message, file=sys.stderr)
     exit
-
+# Établit le blast_record avec le fichier
 with open(file, "r") as file:
     blast_record = NCBIXML.read(file)
 
-possible_hsp = []
+# Trouver les alignements valides et trouver le plus long
+longest_align = 0
+possible_align = []
+
 for alignement in blast_record.alignments:
     for hsp in alignement.hsps:
         if hsp.identities / hsp.align_length == 1:
-            possible_hsp.append(hsp)
+            possible_align.append(alignement)
 
-longest_align = 0
-for hsp in possible_hsp:
-    if hsp.align_length > longest_align:
-        best_hsp = hsp
+for alignement in possible_align:
+    for hsp in alignement.hsps:
+        if hsp.align_length > longest_align:
+            longest_align = hsp.align_length
+            symbol = alignement.title.split("|")[3]
+
+# Construire l'URL du xref
+url = "http://rest.ensembl.org/xrefs/symbol/{species}/{symbol}"
+opt = ["content-type=application/json", "feature=gene"]
+species = "homo_sapiens"
+
+url_xrefs = "{}?{}".format(url, ";".join(opt)).format(
+species= species, symbol= symbol
+)
+
+xref = get_request(url_xrefs)
+
+# Construire l'URL du overlap
+id = xref[0].get("id")
+url = "http://rest.ensembl.org/overlap/id/{id}"
+opt = ["?content-type=application/json;feature=variation"]
+
+url_overlap = "{}{}".format(url, ";".join(opt)).format(
+    id = id,
+)
+
+overlap = get_request(url_overlap)
+
+# Établir le DataFrame avec les variables nécessaire au CSV et l'imprimer au fichier
+df = pd.DataFrame.from_dict(overlap)
+
+variables = {"name":"id","chromosome":"seq_region_name","start":"start","end":"end",
+
+"alleles":"alleles", "consequence":"consequence_type", "clinical":"clinical_significance"
+}
+
+df = df[list(variables.values())]
+df.columns = list(variables.keys())
+
+df.to_csv("./{}.variations.csv".format(id))
